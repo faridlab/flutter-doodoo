346c7cb38122a5d431db4eb601dae0247fedcf7a
Farid Hidayat <e.faridhidayat@gmail.com>
2020-11-24 15:13:05 +0700

    Routers of app

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+import '../views/pages/home/home.dart';
 
+// FIXME: make this router more clear
+class RouterApp {
+  BuildContext context;
+
+  RouterApp({this.context});
+
+  final routes = {
+    '/': (context) => HomeTodoApp(),
+    // '/task': () => {},
+  };
+}
