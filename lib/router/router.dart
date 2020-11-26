02605c5ad2d23b28748ac05e629b464a6694d2e6
Farid Hidayat <e.faridhidayat@gmail.com>
2020-11-26 17:14:00 +0700

    dynamic routes generate

---
+++ lib/router/router.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import './routes.dart';
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  var route;
+  try {
+    route = routes.firstWhere((item) {
+      // FIXME: find path by pattern
+      return item['path'] == settings.name;
+    });
+
+    return MaterialPageRoute(
+        builder: (BuildContext context) => route['screen']());
+  } catch (e) {
+    route = routes.firstWhere((item) {
+      return item['path'] == '*';
+    });
+
+    return MaterialPageRoute(
+        builder: (BuildContext context) => route['screen']());
+  }
+}
